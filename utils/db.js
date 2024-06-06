5f36bc5ef0a5a4ce4603a770d4991feff4159602
Shafi Akinropo <sakinropo@gmail.com>
2024-06-06 10:31:25 +0100

    Update Env

--- utils/db.js
+++ utils/db.js
@@ -6,6 +6,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 });
 
 // Synchronize the models with the database
-sequelize.sync();
+sequelize
+	.sync({ alter: true }) // Use `alter` cautiously, ideally in development only
+	.then(() => {
+		console.log("Database synced!");
+	})
+	.catch((error) => {
+		console.error("Failed to sync database:", error);
+	});
 
 export default sequelize;
